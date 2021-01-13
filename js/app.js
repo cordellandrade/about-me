0a7694cf2069356c006bd33d87c81ad9218abbf9
Cordell Andrade <cordellandrade93@gmail.com>
2021-01-12 23:58:35 -0500

    Created repo for about me lab

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('Hello 201!')
+
+var userName = prompt('Hello, what\'s your name?');
+alert(`Hello ${userName}, nice to meet you!`);
+
+var questionOne = prompt('?').toLowerCase();
+
+console.log('answer');
+
+if (questionOne === 'yes' || questionOne === 'y'); {
+    console.log('you are correct!');
+
+};
