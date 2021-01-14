e76cedcae3f65d128fe7d77c3eaf3a79ff6456c3
Cordell Andrade <cordellandrade93@gmail.com>
2021-01-13 21:27:22 -0500

    Did Css styling and finished questions for lab

--- js/app.js
+++ js/app.js
@@ -1,15 +1,64 @@
 'use strict';
 
-console.log('Hello 201!')
+console.log('Hello 201!');
 
-var userName = prompt('Hello, what\'s your name?');
+var userName = prompt('Hello, what is your name?');
+// console.log(userName);
 alert(`Hello ${userName}, nice to meet you!`);
 
-var questionOne = prompt('?').toLowerCase();
+var questionOne = prompt('Do I live in the US?').toLowerCase();
+// console.log(questionOne);
 
-console.log('answer');
-
-if (questionOne === 'yes' || questionOne === 'y'); {
-    console.log('you are correct!');
-
+if (questionOne === 'yes' || questionOne === 'y') {
+    // console.log('You are wrong, sorry.')
+    alert('You are wrong, sorry.')
+} else if (questionOne === 'no' || questionOne === 'n') {
+    // console.log('You are correct!')
+    alert('You are correct!')
 };
+
+var questionTwo = prompt('Did I live in the US before?').toLowerCase();
+// console.log(questionTwo);
+
+if (questionTwo === 'yes' || questionTwo === 'y') {
+    // console.log('You are correct!')
+    alert('You are correct!')
+} else if (questionTwo === 'no' || questionTwo === 'n') {
+    // console.log('You are wrong, sorry.')
+    alert('You are wrong, sorry.')
+};
+
+var questionThree = prompt('Do I enjoy sports?').toLowerCase();
+// console.log(questionThree);
+
+if (questionThree === 'yes' || questionThree === 'y') {
+    // console.log('You are correct!')
+    alert('You are correct!')
+} else if (questionThree === 'no' || questionThree === 'n') {
+    // console.log('You are wrong, sorry.')
+    alert('You are wrong, sorry.')
+};
+
+var questionFour = prompt('Do I love dogs?').toLowerCase();
+// console.log(questionFour);
+
+if (questionFour === 'yes' || questionFour === 'y') {
+    // console.log('You are correct!')
+    alert('You are correct!')
+} else if (questionFour === 'no' || questionFour === 'n') {
+    // console.log('You are wrong, sorry.')
+    alert('You are wrong, sorry.')
+};
+
+var questionFive = prompt('Is Star Wars one of my favorite trilogies?').toLowerCase();
+// console.log(questionFive);
+
+if (questionFive === 'yes' || questionFive === 'y') {
+    // console.log('You are correct!')
+    alert('You are correct!')
+} else if (questionFive === 'no' || questionFive === 'n') {
+    // console.log('You are wrong, sorry.')
+    alert('You are wrong, sorry.')
+};
+
+alert(`Thank you ${userName} for visiting my website :)`);
